f3a6122df01ef7ecdc6e5c5322baeda0a27bed01
mishrasweta-0503 <m.swetab14@gmail.com>
2026-06-03 23:46:59 +0400

    Refactor security: migrate all local hardcoded secret keys to hidden environment variables

--- fixit/settings.py
+++ fixit/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-y6ocrt!wuwen8k2_7bl4w0x16_2@7mxj2pq*-9wdig7adf9rbe')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
